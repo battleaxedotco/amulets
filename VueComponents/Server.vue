5396c0cc4daeff7cdf8638645a59c1d5b34e72d0
Adam Plouff <adamplouff@gmail.com>
2020-05-12 16:19:32 -0400

    Server.vue component

---
+++ VueComponents/Server.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="server" >
+    </div>
+</template>
+
+<script>
+import amulets from 'amulets'
+
+export default {
+    props: {
+        devName: {
+            type: String,
+            default: 'BattleAxe'
+        },
+        scriptName: {
+            type: String,
+            default: 'Timelord'
+        },
+        port: {
+            type: Number,
+            default: 4000
+        },
+    },
+    data: () => ({
+    }),
+    methods: {
+    },
+    mounted() {
+        amulets.configure({devName: this.devName, scriptName: this.scriptName})
+        amulets.newServer(this.port)
+    },
+}
+</script>
